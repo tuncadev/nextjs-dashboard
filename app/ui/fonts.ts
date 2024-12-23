7bde1a83aed59c9e38cd2186e3f0f913dac8f914
tuncatech <tunca.development@gmail.com>
2024-12-23 15:24:05 +0200

    font fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import { Open_Sans } from 'next/font/google';
+
  
 export const inter = Inter({ subsets: ['latin'] });
 export const openSans = Open_Sans({ subsets: ['latin'] });
